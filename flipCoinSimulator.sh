c78e273c9d8dec0a9d532f6b6d55461afb1b206e
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-21 11:45:31 +0530

    Added loop for coin flipping multiple times and showing number of wins

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,13 +3,26 @@ echo "Welcome"
 
 heads=1
 tails=0
-
-flipCoin=$((RANDOM%2))
-
-if [ $flipCoin -eq $heads ]
-then
-	echo "Heads is the Winner!"
-elif [ $flipCoin -eq $tails ]
-then
-	echo "Tails is the Winner!"
-fi
+headsWin=0
+tailsWin=0
+while [[ $choice -eq 'n' ]]
+do
+	flipCoin=$((RANDOM%2))
+	read -p "Flip Coin ??(y/n)=" choice;
+	case $choice in
+		y)
+			if [ $flipCoin -eq $heads ]
+			then
+				echo "Heads is the Winner!"
+				((headsWin++))
+				echo "Heads has won "$headsWin" times!"
+			elif [ $flipCoin -eq $tails ]
+			then
+				echo "Tails is the Winner!"
+				((tailsWin++))
+				echo "Tails has won $tailsWin times!"
+			fi;;
+		n)
+			exit;;
+	esac
+done
