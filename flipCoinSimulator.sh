d7d4c058e2de1c539e6e3cfeee4664afefc7dc44
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-21 11:22:31 +0530

    Added coin flip to display heads or tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,1 +1,15 @@
+
 echo "Welcome"
+
+heads=1
+tails=0
+
+flipCoin=$((RANDOM%2))
+
+if [ $flipCoin -eq $heads ]
+then
+	echo "Heads is the Winner!"
+elif [ $flipCoin -eq $tails ]
+then
+	echo "Tails is the Winner!"
+fi
